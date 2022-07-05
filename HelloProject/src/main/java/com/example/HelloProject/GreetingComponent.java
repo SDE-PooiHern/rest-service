9ec330eabc2624c3ddf5e70e52076799a20c8f6d
SDPHMOK <SDPHMOK@N5856.slake.sssb.com>
2022-07-05 16:16:42 +0800

    Initial commit

---
+++ HelloProject/src/main/java/com/example/HelloProject/GreetingComponent.java
@@ -0,0 +1,10 @@
+package com.example.HelloProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingComponent {
+	public String getMessage() {
+		return "Hello from GreetingComponent";
+	}
+}
